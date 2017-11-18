084b9f99a3bf8569a38b4c63648235c9e89b8cfe
Daryl Fung <daryl_chief@hotmail.com>
2017-11-17 23:31:15 -0600

    added more rules

--- expert_sys.pl
+++ expert_sys.pl
@@ -14,6 +14,8 @@ rule(( meetup_location(clubs) :-
 	hobby(socialize),  TODO), 100).
 rule(( meetup_location(bar) :-
 	hobby(socialize),  TODO), 100).
+rule(( meetup_location(student_organization) :-
+	hobby(socialize), TODO), 100).
 
 %%%%%%%%%%%%%%%%%%
 
@@ -24,25 +26,59 @@ rule(( hobby(studying) :-
 	personality_type(self_improving),  TODO), 100).
 rule(( hobby(studying) :-
 	personality_type(artistic),  TODO), 100).
+rule(( hobby(music) :-
+	personality_type(artistic),  TODO), 100).
 rule(( hobby(socialize) :-
 	personality_type(extravert),  TODO), 100).
 rule(( hobby(socialize) :-
 	personality_type(self_improving),  TODO), 100).
+rule(( hobby(socialize) :-
+	personality_type(spontaneuous),  TODO), 100).
 
 %%%%%%%%%%%%%%%%%%
 
 %personality_type%
 rule(( personality_type(self_improving) :-
-	TODO,  TODO), 100).
-%
+	person_description(independent),  TODO), 70).
+rule(( personality_type(self_improving) :-
+	person_description(curiousity),  TODO), 90).
+rule(( personality_type(self_improving) :-
+	person_description(consistent),  TODO), 90).
 rule(( personality_type(artistic) :-
+	person_description(curiousity),  TODO), 100).
+rule(( personality_type(artistic) :-
+	person_description(innovative),  TODO), 100).
+rule(( personality_type(extravert) :-
+	person_description(energetic),  TODO), 100).
+rule(( personality_type(extravert) :-
+	person_description(independent),  TODO), 100).
+rule(( personality_type(spontaneuous) :-
+	person_description(enthusiastic),  TODO), 100).
+rule(( personality_type(spontaneuous) :-
+	person_description(energetic),  TODO), 100).
+
+%%%%%%%%%%%%%%%%%%
+
+
+%person_description%
+rule(( person_description(independent) :-
 	TODO,  TODO), 100).
 %
-rule(( personality_type(extravert) :-
+rule(( person_description(curiousity) :-
+	TODO,  TODO), 100).
+%
+rule(( person_description(innovative) :-
+	TODO,  TODO), 100).
+%
+rule(( person_description(enthusiastic) :-
+	TODO,  TODO), 100).
+%
+rule(( person_description(consistent) :-
 	TODO,  TODO), 100).
 %
 
-%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%
 
 
 askable(like improving).
